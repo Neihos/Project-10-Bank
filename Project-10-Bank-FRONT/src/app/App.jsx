75547553db7d15fb2c7d3e186e3202c400d73741
Neihos <detourbe.nathanael@gmail.com>
2025-12-20 13:16:05 +0100

    add home page

--- Project-10-Bank-FRONT/src/app/App.jsx
+++ Project-10-Bank-FRONT/src/app/App.jsx
@@ -2,11 +2,13 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <Provider store={store}>
       <Header />
+      <Home />
       <Footer />
     </Provider>
   );
