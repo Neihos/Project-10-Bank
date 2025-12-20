9b638153f77a2a12cc3ed718484b68b3720e35d4
Neihos <detourbe.nathanael@gmail.com>
2025-12-20 15:58:58 +0100

    add user page

--- Project-10-Bank-FRONT/src/app/App.jsx
+++ Project-10-Bank-FRONT/src/app/App.jsx
@@ -2,13 +2,13 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import SignIn from "./pages/SignIn";
+import User from "./pages/User";
 
 export default function App() {
   return (
     <Provider store={store}>
       <Header />
-      <SignIn />
+      <User />
       <Footer />
     </Provider>
   );
