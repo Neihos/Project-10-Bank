7e9abefce9bf50733d12c0183ed4894c9809f31a
Neihos <detourbe.nathanael@gmail.com>
2025-12-23 10:37:14 +0100

    add logout

--- Project-10-Bank-FRONT/src/app/components/Header.jsx
+++ Project-10-Bank-FRONT/src/app/components/Header.jsx
@@ -1,21 +1,45 @@
 import logo from "../../assets/images/argentBankLogo.webp";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../store/authSlice";
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
   return (
     <nav className="main-nav">
-      <a href="/" className="main-nav-logo">
+      <Link to="/" className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src={logo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a href="/login" className="main-nav-item">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </a>
+        {!token ? (
+          <Link to="/login" className="main-nav-item">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        ) : (
+          <button
+            type="button"
+            className="main-nav-item"
+            onClick={handleLogout}
+            style={{ background: "none", border: "none", cursor: "pointer" }}
+          >
+            <i className="fa fa-sign-out"></i>
+            Sign Out
+          </button>
+        )}
       </div>
     </nav>
   );
